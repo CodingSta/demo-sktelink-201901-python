5ceb911bc89a4ae3653bb07b7ba236b6aef01657
Chinseok Lee <me@askcompany.kr>
2019-01-25 12:41:40 +0900

    오전반: 텔레그램 명령 구현 (단어수세기, 네이버실시간검색어)

--- 오전반/telegram_server.py
+++ 오전반/telegram_server.py
@@ -1,6 +1,24 @@
 from telegram.ext import Updater, Filters
 from telegram.ext import CommandHandler, ConversationHandler, MessageHandler
 
+import requests
+from bs4 import BeautifulSoup
+
+def 네이버_실시간_검색어():
+    url = 'http://naver.com'
+    응답 = requests.get(url)
+    html = 응답.text
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    tag_list = soup.select('.PM_CL_realtimeKeyword_rolling .ah_k')
+
+    keyword_list = []
+    for tag in tag_list:
+        keyword_list.append(tag.text)
+
+    return keyword_list
+
 
 def start(bot, update):
     chat_id = update.message.chat_id
@@ -12,10 +30,26 @@ def echo(bot, update):
     chat_id = update.message.chat_id
     text = update.message.text
 
-    if text == '야':
+    if text.startswith('단어수 세어줘:'):
+        문자열 = text[8:]
+        단어수 = len(문자열.split())  # int 타입
+        response = '단어수는 {}개입니다.'.format(단어수)
+
+    elif text == '네이버 실검':
+        keyword_list = 네이버_실시간_검색어()
+        response = '\n'.join(keyword_list)
+
+    elif text.startswith('네이버에서 찾아줘:'):
+        검색어 = text[10:]
+        # TODO: 네이버에서 검색결과를 찾을 수 있어요. !!!
+        response = '검색결과가 없습니다.'
+
+    elif text == '야':
         response = '왜?'
+
     elif text == '네이버에서 맛집 찾아줘.':
         response = '니가 찾아봐. http://naver.com'
+
     else:
         response = '니가 무슨 말 하는 지 모르겠어.'
 
